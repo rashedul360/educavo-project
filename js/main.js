7a3502f9ac70c79183f976865a5d034673e4e10f
md rashedul islam <rashedul360@gmail.com>
2021-07-24 07:30:14 +0600

    add all

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+// select header 
+let selectHeader = document.querySelector("#header");
+
+// sticky menu
+window.onscroll=()=>{
+    // Add class
+    if(this.scrollY > 100){
+        selectHeader.classList.add("sticky");
+    // Remove class
+    }else{
+        selectHeader.classList.remove("sticky");
+    }
+}
+
+
